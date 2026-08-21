31dc0e4d708d7da553ab93ac9aaa9e5050996d74
Maxwell Selassie <maxwellselassie2004@gmail.com>
2026-08-21 16:32:44 +0000

    Fix typo in RAW_DATA_DIR variable and ensure newline at end of file in config.py

--- configs/config.py
+++ configs/config.py
@@ -4,7 +4,8 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
 DATA_DIR = PROJECT_ROOT / "data"
 
-RAW_DATA_DR = DATA_DIR / "raw"
+RAW_DATA_DIR = DATA_DIR / "raw"
 BRONZE_DATA_DIR = DATA_DIR / "bronze"
 SILVER_DATA_DIR = DATA_DIR / "silver"
 GOLD_DATA_DIR = DATA_DIR / "gold"
+AUDIT_DATA_DIR = DATA_DIR / "audit"
